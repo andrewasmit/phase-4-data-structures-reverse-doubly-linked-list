212dde9aae483fc70333e5f7520b7d63389f4834
andrew <andrewasmit@hotmail.com>
2023-03-31 11:29:27 -0500

    commit tree e87b9420ab889c905895ee3bdb8208e87669b3e4

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,5 @@
 require_relative './node'
+require 'pry'
 
 class LinkedList
   attr_accessor :head, :tail
@@ -9,6 +10,19 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    return if self.head == self.tail
+
+    curr = self.tail 
+    old_head = self.head
+    self.head = curr
+    until curr == nil
+      old_prev = curr.prev_node
+      old_next = curr.next_node
+      curr.prev_node = old_next
+      curr.next_node = old_prev
+      curr = old_prev
+    end
+    self.tail = old_head
   end
+
 end
